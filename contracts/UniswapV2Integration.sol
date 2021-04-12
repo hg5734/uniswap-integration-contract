46a793754caa7c36914848d8924609d982f7cdaa
hg-tranche <himanshugoyal5734@gmail.com>
2021-04-12 23:22:32 +0400

    added various swap functions

--- contracts/UniswapV2Integration.sol
+++ contracts/UniswapV2Integration.sol
@@ -8,6 +8,7 @@
 pragma solidity ^0.6.12;
 
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+
 import "./interface/IUniswap.sol";
 
 contract UniswapV2Integration {
@@ -17,14 +18,43 @@ contract UniswapV2Integration {
         uniswap = IUniswap(_uniswap);
     }
 
-    function swapTokensForEth(address token, uint amountIn, uint amountOutMin, uint deadline, address path1) external {
+    function swapTokensForEth(address token, uint amountIn, uint amountOutMin, uint deadline) public {
         IERC20(token).transferFrom(msg.sender, address(this), amountIn);
         address[] memory path = new address[](2);
-        path[0] = address(path1);
+        path[0] = token;
         path[1] = uniswap.WETH();
         IERC20(token).approve(address(uniswap), amountIn);
         uniswap.swapExactTokensForETH(
           amountIn,
+          amountOutMin,
+          path,
+          msg.sender,
+          deadline
+        );
+    }
+
+     function swapTokensForToken(address token, address tokenOut, uint amountIn, uint amountOutMin, uint deadline) public {
+        IERC20(token).transferFrom(msg.sender, address(this), amountIn);
+        address[] memory path = new address[](2);
+        path[0] = token;
+        path[1] = tokenOut;
+        IERC20(token).approve(address(uniswap), amountIn);
+        uniswap.swapExactTokensForTokens(
+          amountIn,
+          amountOutMin,
+          path,
+          msg.sender,
+          deadline
+        );
+    }
+
+     function swapEthForToken(address token, uint amountIn, uint amountOutMin, uint deadline) public {
+        IERC20(token).transferFrom(msg.sender, address(this), amountIn);
+        address[] memory path = new address[](2);
+        path[0] = address(token);
+        path[1] = uniswap.WETH();
+        IERC20(token).approve(address(uniswap), amountIn);
+        uniswap.swapExactETHForTokens(
           amountOutMin,
           path,
           msg.sender,
